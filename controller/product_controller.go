fc23521b6071f403ea00c0a2561d8fdc580c05c2
Sarah Júlia <129016417+sarahjl1909@users.noreply.github.com>
2025-04-13 20:50:33 -0400

    Rota: Buscar produtos

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -1,8 +1,28 @@
-package model
+package controller
+
+import (
+	"go-api/model"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type productController struct {
 }
 
 func NewProductController() productController {
 	return productController{}
+}
+
+func (p *productController) GetProducts(ctx *gin.Context) {
+
+	products := []model.Product{
+		{
+			ID:    1,
+			Name:  "Batata frita",
+			Price: 20,
+		},
+	}
+
+	ctx.JSON(http.StatusOK, products)
 }
